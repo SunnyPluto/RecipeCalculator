534dcfb18943185d2b36a5151e91953192a3b4a4
Alex <Sunny4ka@andrews-mac-mini.local>
2017-06-06 19:05:01 -0700

    Initial Commit

---
+++ Food FormUla/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food FormUla
+//
+//  Created by Sunny4ka on 6/6/17.
+//  Copyright © 2017 Sunny4ka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
